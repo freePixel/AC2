36e3fecaf1f9bb5ca8992715fda02d0e1a4deb3d
Tiago <tiagoa3203@gmail.com>
2022-03-20 16:26:36 +0000

    Ex5 resolvido

--- aula03/parte1/5/5.s
+++ aula03/parte1/5/5.s
@@ -14,7 +14,7 @@
 
 
 #I/O
-.equ SFT_BASE_HI , 0xBF88
+.equ SFR_BASE_HI , 0xBF88
 
 .equ TRISB , 0x6040
 .equ PORTB  , 0x6050
@@ -39,5 +39,57 @@
 
 .globl main
 
+
+#$s1 , v
+
 main:
 	
+	addiu $sp , $sp , -8
+	sw $ra , 0($sp)
+	sw $s0 , 4($sp)
+
+	li $s0 , 0 							#int v = 0;
+
+	lui $t0 , SFR_BASE_HI
+	lw $t1  , TRISE($t0)
+	andi $t1 , $t1 , 0xfffe
+	sw $t1 , TRISE($t0)					#TRISE0 = 0
+
+while:									#while(1){
+
+	lui $t0 , SFR_BASE_HI
+	lw $t1 , LATE($t0)
+	andi $t1 , $t1 , 0xfffe			
+
+	move $t2 , $s0
+	andi $t2 , $t2 , 0x0001				#(OPCIONAL, IMPEDE A ESCRITA EM LATE EM POSIÇÕES DIFERENTES DE 0)
+
+	or $t1 , $t1 , $t2					#merge
+
+	sw $t1 , LATE($t0)					#LATE0 = v;
+
+	li $a0 , 500
+	jal delay 							#delay(500);
+
+	xori $s0 , $s0 , 0x0001 			# v ^= 1;
+
+	j while 							#}
+
+	lw $ra , 0($sp)
+	lw $s0 , 4($sp)
+
+	jr $ra   							#}
+
+delay:
+    li $v0 , RESET_CORE_TIMER
+    syscall
+    delay_while:
+    li $v0 , READ_CORE_TIMER
+    syscall
+    mul $v1 , $a0 , 20000
+    bge $v0 , $v1 , delay_ewhile
+    j delay_while
+    delay_ewhile:
+    jr $ra
+                
+
