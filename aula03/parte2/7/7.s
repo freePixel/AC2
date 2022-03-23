026d6be1a65fe316388eb623c2fbdef89aa195f4
tiago <tiagoa3203@gmail.com>
2022-03-23 09:49:14 +0000

    (aula3) Ex 7

--- aula03/parte2/7/7.s
+++ aula03/parte2/7/7.s
@@ -1,16 +1,16 @@
 #SYSCALLS
-.equ IN_KEY		, 1
-.equ GET_CHAR		, 2
-.equ PUT_CHAR		, 3
-.equ READ_INT		, 4
-.equ READ_INT10		, 5
-.equ PRINT_INT		, 6
-.equ PRINT_INT10	, 7
-.equ PRINT_STR		, 8
-.equ READ_STR		, 9
-.equ EXIT		, 10
-.equ READ_CORE_TIMER	, 11
-.equ RESET_CORE_TIMER  	, 12
+.equ IN_KEY     , 1
+.equ GET_CHAR       , 2
+.equ PUT_CHAR       , 3
+.equ READ_INT       , 4
+.equ READ_INT10     , 5
+.equ PRINT_INT      , 6
+.equ PRINT_INT10    , 7
+.equ PRINT_STR      , 8
+.equ READ_STR       , 9
+.equ EXIT       , 10
+.equ READ_CORE_TIMER    , 11
+.equ RESET_CORE_TIMER   , 12
 
 
 #I/O
@@ -38,14 +38,73 @@
 .text
 
 .globl main
-
+#$s0 -> contador
 main:
-	
+    
     li $t0 , SFR_BASE_HI
-    li $t1 , TRISE($t0)
+    lw $t1 , TRISE($t0)
     andi $t1 , $t1 , 0xfff0
     sw $t1 , TRISE($t0)         #RE[0..4] OUTPUTS
 
-    li $t1 , TRISB($t0)
+    lw $t1 , TRISB($t0)
     ori $t1 , $t1 , 0x000f
     sw $t1 , TRISB($t0)        #RB[0..4] INPUTS
+
+while:                          #{
+
+    
+    li $t0 , SFR_BASE_HI
+    lw $t1 , LATE($t0)
+    andi $t1 , $t1 , 0xfff0
+    andi $s0 , $s0 , 0x000f     #evita a escrita em RB4+ e impede o contador de ultrapassar 0xf
+    or $t1 , $t1 , $s0
+    sw $t1 , LATE($t0)          #write counter
+
+
+    lw $t1 , PORTB($t0)
+    andi $t1 , $t1 , 0x0004
+    beq $t1 , 0x0004 , else
+
+    sll $s0 , $s0 , 1
+    srl $v0 , $s0 , 3
+    andi $v0 , $v0 , 0x0001
+    xori $v0 , $v0 , 0x0001
+    or $s0 , $s0 , $v0          #(left shift)
+
+    j endif
+
+else:
+
+    srl $s0 , $s0 , 1           #(right shift)
+    
+    sll $v0 , $s0 , 3
+    andi $v0 , $v0 , 0x0008
+    xori $v0 , $v0 , 0x0008     #negar bit
+    or $s0 , $s0 , $v0          #merge
+
+
+endif:
+
+
+    li $v0 , 667
+    jal delay                   #delay(667) ~1.5hZ;
+
+    j while                     #}
+
+
+    jr $ra
+
+
+delay:
+    li $v0 , RESET_CORE_TIMER
+    syscall
+    delay_while:
+    li $v0 , READ_CORE_TIMER
+    syscall
+    mul $v1 , $a0 , 20000
+    bge $v0 , $v1 , delay_ewhile
+    j delay_while
+    delay_ewhile:
+    jr $ra
+                
+
