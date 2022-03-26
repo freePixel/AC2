df9ddd03e76e2c5ea30b41da761f4bc281c2e8e6
Tiago <tiagoa3203@gmail.com>
2022-03-26 12:42:40 +0000

    aula04/parte1/1 resolvido

---
+++ aula04/parte1/1/1.c
@@ -0,0 +1,26 @@
+#define __LANGUAGE_C__ //se o intelliSense não reconhecer __PORTEbits_t , ...  (ignorar aviso gerado com o pcompile)
+#include <detpic32.h>  //se o IDE nao detetar <detpic32.h> deve-se adicionar "/opt/pic32mx/include" no include path
+
+#define K 20000
+
+void delay(unsigned int ms)
+{
+    resetCoreTimer();
+    while(readCoreTimer() < K * ms);
+} 
+
+int main(void)
+{
+    __TRISCbits_t TRISCbits;
+    __LATCbits_t  LATCbits;
+
+    TRISCbits.TRISC14 = 0;
+
+    while(1)
+    {
+        delay(500);
+        LATCbits.LATC14 = !LATCbits.LATC14;
+    }
+    return 0;
+    
+}
